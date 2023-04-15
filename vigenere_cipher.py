02733adedf9ab87552cf553f21a5e56b66f4ec04
ChristianJames Luces <christianjames231103@gmail.com>
2023-04-15 22:26:30 +0800

    Pseudocode

---
+++ vigenere_cipher.py
@@ -0,0 +1,14 @@
+# 2:Pseudocode
+# Define variables
+# Display welcome message and its function
+# Ask for user input
+# Translate the letters in message to its corresponding numbers (0-25)
+# Translate the letters in key to its corresponding numbers (0-25)
+# Calculate Add, Mod, and Ciphertext
+# Print the output
+# Asking the user if they want to try it again
+# If the user want to try it again 
+# If the user do not want to try it again
+# If the user did not input a valid answer
+# Exit the loop if they do not want to try it again
+# Go back to the start of the loop
